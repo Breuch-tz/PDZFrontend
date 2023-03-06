0c99796a69d8a2e1ddb6591ce6707d8b9aa0bb6f
Breuch-tz <99825743+Breuch-tz@users.noreply.github.com>
2023-03-06 18:03:39 +0100

    viel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProductComponent } from './components/product/product.component';
+
+import { ImpressumComponent } from './components/impressum/impressum.component';
+import { DatenschutzComponent } from './components/datenschutz/datenschutz.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,6 +15,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'products', component: ProductComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'datenschutzerklaerung', component: DatenschutzComponent },
 ];
 
 @NgModule({
@@ -21,4 +27,6 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  constructor() {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,24 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProductComponent } from './components/product/product.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { UhrenSchmuckComponent } from './components/products/uhren-schmuck/uhren-schmuck.component';
-import { TrauringeComponent } from './components/products/trauringe/trauringe.component';
-import { GoldschmiedeComponent } from './components/products/goldschmiede/goldschmiede.component';
-import { WerkstattComponent } from './components/products/werkstatt/werkstatt.component';
 
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgImageSliderModule } from 'ng-image-slider';
 import { MatDividerModule } from '@angular/material/divider';
+import { DatenschutzComponent } from './components/datenschutz/datenschutz.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ProductComponent,
     ContactComponent,
-    UhrenSchmuckComponent,
-    TrauringeComponent,
-    GoldschmiedeComponent,
-    WerkstattComponent,
+    ProductComponent,
+    DatenschutzComponent,
+    ImpressumComponent,
   ],
   imports: [
     BrowserModule,
